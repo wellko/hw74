41fd4c23d5c7e06b66df6d72dc5044d3d223bb3b
Kwon Valerii <wellko24@gmail.com>
2023-01-16 16:24:43 +0600

    reworked writeMessage for creating folder if have no such directory

--- writeMessage.ts
+++ writeMessage.ts
@@ -1,5 +1,6 @@
 import {promises as fs} from 'fs';
 import {message} from "./types";
+import { existsSync } from 'node:fs';
 
 
 
@@ -7,6 +8,9 @@ const writeMessage = async (message: message, date: string) => {
 	const fileName = './messages/' + date + '.txt';
 
 	try {
+		if (!existsSync('./messages')){
+			await fs.mkdir('./messages');
+		}
 		await fs.writeFile(fileName, message.message);
 	} catch (err) {
 		console.error(err);
